f64b56d734a643535d122f91502c89e2bcda7f8c
Anas <anasuddinbiswas@gmail.com>
2024-09-25 11:18:30 +0600

    fixed alert if letters or money less than 1 is given as donation

--- donate.js
+++ donate.js
@@ -3,6 +3,13 @@ document
   .addEventListener("click", function (event) {
     event.preventDefault();
     const donate = inputGatherer("input-noakhali");
+    if (isNaN(donate)) {
+      alert("please enter a valid positive number");
+      return;
+    } else if (donate < "1") {
+      alert("please enter a valid positive number");
+      return;
+    }
     // console.log(donate);
     const balance = textValueGatherer("balance");
     const newBalance = parseFloat(balance) - parseFloat(donate);
@@ -17,6 +24,13 @@ document
   .addEventListener("click", function (event) {
     event.preventDefault();
     const donate1 = inputGatherer("feni-input");
+    if (isNaN(donate1)) {
+      alert("please enter a valid positive number");
+      return;
+    } else if (donate1 < "1") {
+      alert("please enter a valid positive number");
+      return;
+    }
     const balance2 = textValueGatherer("balance");
     const newBalance2 = parseFloat(balance2) - parseFloat(donate1);
     document.getElementById("balance").innerText = newBalance2;
@@ -29,6 +43,13 @@ document
   .addEventListener("click", function (event) {
     event.preventDefault();
     const donate2 = inputGatherer("stu-input");
+    if (isNaN(donate2)) {
+      alert("please enter a valid positive number");
+      return;
+    } else if (donate2 < "1") {
+      alert("please enter a valid positive number");
+      return;
+    }
     const balance3 = textValueGatherer("balance");
     const newBalance3 = parseFloat(balance3) - parseFloat(donate2);
     document.getElementById("balance").innerText = newBalance3;
